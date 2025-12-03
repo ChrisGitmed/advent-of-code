f3dd51f108bb478026ac18443e104271a6689e46
Chris Gitmed <chrisgitmed@Chriss-MacBook-Pro-2.local>
2025-12-02 17:48:13 -0800

    Optimized day one, solution one

--- 2025/1/solutions.js
+++ 2025/1/solutions.js
@@ -5,37 +5,19 @@
  * @returns {number}
  */
 export const solutionOne = (input) => {
-  // Define a circular map from 0-99
-  const dict = [];
-  for (let i = 0; i < 100; i += 1) {
-    dict.push(i);
-  }
+  let password = 0;
+  let currentDialPosition = 50;
 
-  // Create a variable to track the zero counts
-  let zeroCount = 0;
-
-  // The dial starts pointing at 50
-  let dialCurrentPosition = 50;
-
-  // For each line
   for (const row of input) {
-    // Extract the direction and movement count from the raw string
-    const direction = row.substring(0, 1);
-    const moveCount = row.substring(1);
-    const moveNumber = Number(moveCount);
-
-    // Conditionally move the dial either left or right
-    if (direction === 'L') {
-      dialCurrentPosition -= (moveNumber % 100);
-      if (dialCurrentPosition < 0) dialCurrentPosition += 100;
+    const moveNumber = Number(row.substring(1));
+    if (row.substring(0, 1) === 'L') {
+      currentDialPosition -= (moveNumber % 100);
+      if (currentDialPosition < 0) currentDialPosition += 100;
     } else {
-      dialCurrentPosition += (moveNumber % 100);
-      if (dialCurrentPosition >= 100) dialCurrentPosition -= 100;
+      currentDialPosition = (currentDialPosition + moveNumber) % 100;
     }
-
-    // If the dial is at 0, add one to our answer
-    if (dialCurrentPosition === 0) zeroCount += 1;
+    if (currentDialPosition === 0) password += 1;
   }
 
-  return zeroCount;
+  return password;
 };
